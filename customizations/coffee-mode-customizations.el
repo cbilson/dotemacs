d229782c36bd3c9e59694d11b582414ef207816e
Chris Bilson <cbilson@pobox.com>
2011-07-11 08:45:31 -0700

    fix coffee mode customization

--- customizations/coffee-mode-customizations.el
+++ customizations/coffee-mode-customizations.el
@@ -1,13 +1,18 @@
+(autoload 'coffee-mode "coffee-mode" t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
-(defun use-jcoffee-script ()
+(defun use-jcoffee-compiler ()
   (let ((jcoffee-jar (concat vendor-directory "/jcoffeescript-1.1.jar")))
     (setq coffee-command (concat "java -jar " jcoffee-jar))
     (setq coffee-args-compile "")))
 
+(defun use-iron-coffee-compiler ()
+  (let ((iron-coffee-exe (concat vendor-directory "/IronCoffee.exe")))
+    (setq coffee-command iron-coffee-exe)))
+
 (defun coffee-custom ()
   "coffee-mode-hook"
-  (use-jcoffee-script))
+  (use-iron-coffee-compiler))
 
 (add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
