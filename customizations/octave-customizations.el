cd9d1c10b62b8559a15a1999f9e60dc3eac93c51
Chris Bilson <cbilson@pobox.com>
2011-12-06 08:28:13 -0800

    use my personal octave on mac

--- customizations/octave-customizations.el
+++ customizations/octave-customizations.el
@@ -1,4 +1,7 @@
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
+
 (setq inferior-octave-program
       (if (eq system-type 'windows-nt)
           "C:/Octave/3.2.4_gcc-4.4.0/bin/octave.exe"
-        "octave"))
+        "~/bin/octave"))
+
