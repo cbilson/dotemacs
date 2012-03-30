7ef0e3f36789721d4d9c22813e693845f313e03d
Chris Bilson <cbilson@pobox.com>
2012-03-30 07:10:26 -0700

    fix clojure customizations to not eval clojurescript buffers

--- customizations/clojure-customizations.el
+++ customizations/clojure-customizations.el
@@ -2,29 +2,35 @@
 ;; clojure stuff
 ;;
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojurescript-mode))
 
-(eval-after-load "clojure-mode"
-  '(progn (add-hook 'clojure-mode-hook
-                    '(lambda ()
-                       (require 'paredit)
+(add-hook 'clojure-mode-hook
+          '(lambda ()
+             (require 'durendal)
+             (durendal-enable)
+
+             (require 'clojure-test-mode)
+             (save-excursion
+               (goto-char (point-min))
+               (if (search-forward "(deftest" nil t)
+                   (clojure-test-mode)))
+
+             (require 'midje-mode)
+             (save-excursion
+               (goto-char (point-min))
+               (if (search-forward "(fact" nil t)
+                   (midje-mode)))
+
+             (require 'paredit)
                        (paredit-mode)
-                       
-                       (require 'durendal)
-                       (durendal-enable)
-                       
-                       (require 'clojure-test-mode)
-                       (save-excursion
-                         (goto-char (point-min))
-                         (if (search-forward "(deftest" nil t)
-                             (clojure-test-mode)))
 
-                       (require 'midje-mode)
-                       (save-excursion
-                         (goto-char (point-min))
-                         (if (search-forward "(fact" nil t)
-                             (midje-mode)))
+             (define-clojure-key-maps)))
 
-                       (define-clojure-key-maps)))))
+(add-hook 'clojurescript-mode-hook
+          (lambda () (progn (require 'paredit)
+                       (paredit-mode)
+                       (define-clojure-key-maps))))
 
-(add-hook 'slime-mode-hook (lambda () (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)))
-
+(add-hook 'slime-mode-hook
+          (lambda ()
+            (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)))
